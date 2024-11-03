4c4e80c51e0ee4de2d996a5847f773ce0bdcb78a
Sachini <sachichathudissanayaka@gmail.com>
2024-11-03 21:59:56 +0530

    Add changes

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 import express from "express";
 import connectDb from "./config/db.js";
 import logger from "./config/logger.js";
+import Product from "./models/product.model.js";
 
 const PORT = process.env.PORT || 3000;
 
@@ -22,6 +23,22 @@ connectDb()
 // Middleware
 app.use(express.json());
 
-app.get("/", (req, res) => {
-  res.send("Hi");
+app.post("/products", async (req, res) => {
+  const product = req.body;
+
+  if(!product.name || !product.quantity || !product.price || !product.image){
+    logger.error("Missing required fileds")
+    return res.status(400).json({success:false, message: error.message});
+  }
+
+  const newProduct = new Product(product);
+
+  try {
+    await newProduct.save();
+    logger.info("Product added successfuly");
+    res.status(201).json({success:true, message: "Product added successfuly", data:newProduct});
+  } catch (error) {
+    logger.error("Error adding product")
+    return res.status(500).json({success:false, message: "Server Error"});
+  }
 });
