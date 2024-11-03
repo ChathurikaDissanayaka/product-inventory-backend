e54dda1c7215670fa1e4b6e9c096794a03581844
Sachini <sachichathudissanayaka@gmail.com>
2024-11-03 23:58:14 +0530

    Add changes

--- index.js
+++ index.js
@@ -21,14 +21,40 @@ connectDb()
 // Middleware
 app.use(express.json());
 
+// Get all
+app.get("/api/products", async (req, res) => {
+  try {
+    const products = await Product.find({});
+    logger.info("Products fetched successfuly");
+    res.status(200).json({ success: true, data: products });
+  } catch (error) {
+    logger.error("Error fetching products");
+    res.status(500).json({ success: false, message: "Server Error" });
+  }
+});
+
+// Get a product
+app.get("/api/products/:id", async (req, res) => {
+  try {
+    const {id} = req.params
+    const product = await Product.findById(id);
+    logger.info("Product fetched successfuly");
+    res.status(200).json({ success: true, data: product });
+  } catch (error) {
+    logger.error("Error fetching product");
+    res.status(500).json({ success: false, message: "Server Error" });
+  }
+});
+
+// Add
 app.post("/api/products", async (req, res) => {
   const product = req.body;
 
   if (!product.name || !product.quantity || !product.price || !product.image) {
-    logger.error("Missing required fileds");
+    logger.error("Missing one or more required fields");
     return res
       .status(400)
-      .json({ success: false, message: "Missing required fileds" });
+      .json({ success: false, message: "Missing one or more required fields" });
   }
 
   const newProduct = new Product(product);
@@ -47,4 +73,22 @@ app.post("/api/products", async (req, res) => {
   }
 });
 
-app.delete("/api/products/:id", async (req, res) => {});
+// Delete
+app.delete("/api/products/:id", async (req, res) => {
+  const { id } = req.params;
+
+  try {
+    const product = await Product.findByIdAndDelete(id);
+    if (!product) {
+      logger.error("Product not found");
+      return res
+        .status(404)
+        .json({ success: false, message: "Product not found" });
+    }
+    logger.info("Product deleted successfuly");
+    res.status(200).json({ success: true, message: "Product deleted" });
+  } catch (error) {
+    logger.error("Error deleting product");
+    return res.status(500).json({ success: false, message: "Server Error" });
+  }
+});
