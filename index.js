6cc063dd990b247e433a5412d48fc2ab49b15fd3
Sachini <sachichathudissanayaka@gmail.com>
2024-11-05 23:11:58 +0530

    Add changes

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ import express from "express";
 import connectDb from "./config/db.js";
 import logger from "./config/logger.js";
 import productRoutes from "./routes/product.routes.js";
+import cors from "cors";
 
 const PORT = process.env.PORT || 4000;
 
@@ -20,6 +21,14 @@ connectDb()
 
 // Middleware
 app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(
+  cors({
+    origin: process.env.FRONTEND_URL,
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    allowedHeaders: ["Content-Type"],
+  })
+);
 
 // Routes
 app.use("/api/products", productRoutes);
